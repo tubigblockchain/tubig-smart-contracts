bf03730edcae00aa232fab3a92d8d639ba2e1879
tubigblockchain <35543268+tubigblockchain@users.noreply.github.com>
2018-01-21 23:29:17 +0100

    Add files via upload

---
+++ Tubig.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.12;
+
+import "./lib/BurnableToken.sol";
+import "./lib/UpgradeableToken.sol";
+
+contract Tubig is BurnableToken, UpgradeableToken {
+
+  string public name;
+  string public symbol;
+  uint public decimals;
+
+  function Tubig(address _owner)  UpgradeableToken(_owner) {
+    name = "Tubig";
+    symbol = "TUB";
+    totalSupply = 10000000000;
+    decimals = 2;
+
+    balances[_owner] = totalSupply;
+  }
+}
